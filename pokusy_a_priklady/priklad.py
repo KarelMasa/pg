1ca876b4ad64b12ac4fc71d47371b8f70c50e40e
KarelMasa <masaka00@jcu.cz>
2024-10-13 20:55:00 +0200

    priklady

---
+++ pokusy_a_priklady/priklad.py
@@ -0,0 +1,11 @@
+def cislo_text(cislo):
+    # funkce zkonvertuje cislo do jeho textove reprezentace
+    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
+    return "dvacet pět"
+
+if __name__ == "__main__":
+    nestandardni = {0 : "Nula", 1 : "jedna", 2 : "dva", 3: "tři", 4: "čtyři", 5: "pět", 6:"šest", 7:"sedm", 8:"osm", 9:"devět", 10:"deset", 11:"jedenáct"}
+
+    cislo = input("Zadej číslo: ")
+    text = cislo_text(cislo)
+    print(text)
