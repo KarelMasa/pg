5673fcb31972174d3a7073c0ab11616bbf714f26
KarelMasa <masaka00@jcu.cz>
2024-10-26 14:08:33 +0000

    cv

--- pokusy_a_priklady/cviceni2.py
+++ pokusy_a_priklady/cviceni2.py
@@ -1,7 +1,10 @@
+import time
+
 def vypis_text(text):
     print(f'text je: {text}')
     return 1
 
 if __name__ == "__main__":
-    vystup = input ("Zadej vstup: ")
-    vystup = vypis_text(vstup)
+
+#    vstup = input ("Zadej vstup: ")
+#    vystup = vypis_text(vstup)
