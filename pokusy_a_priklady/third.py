54f45dc083d3b62a2266af44fbc73288951ed855
KarelMasa <masaka00@jcu.cz>
2024-10-28 11:47:30 +0100

    t

--- pokusy_a_priklady/third.py
+++ pokusy_a_priklady/third.py
@@ -1,1 +1,33 @@
+"""
+Ve svém repozitáři vytvořte soubor third.py, jako vzor použijte soubor přiložený k tomuto zadání. Upravte funkci je_prvocislo tak, aby pro jakékoli celé číslo vrátila True pokud je prvočíslo a False pokud není:
+1 -> False
+2 -> True
+3 -> True
+100 -> False
+101 -> True
+Upravte i druhou funkci vrat_prvocisla, které pro zadané maximum vrátí seznam prvočísel mezi 1 a zadaným maximem (včetně):
+1 -> []
+2 -> [2]
+3 -> [2, 3]
+10 -> [2, 3, 5, 7]
+"""
+from math import sqrt
 
+def je_prvocislo(cislo):
+    cisloparam = int(cislo)
+    if cisloparam > 0:
+        odmocnina = sqrt(cisloparam)
+    else:
+        print(f"Zadané číslo: {cisloparam} < 1. Proto náš   ")
+
+
+if __name__ == "__main__":
+    cislo = input("Zadej celé číslo: ")
+    type(cislo)
+    if type(cislo) == str:
+        #cislo = int(cislo)
+        print(f"Zadané číslo: {cislo} ")
+    elif cislo == float:
+        print(f"Zadané číslo: {cislo} není celé číslo")
+    else: print(f"Zadané číslo: {cislo} není celé číslo")
+    
---
+++ pokusy_a_priklady/typ_promenne.py
@@ -0,0 +1,29 @@
+def urcityp(parametr1):
+    #if type(parametr1) == int: print(f"Zadané číslo: {parametr1} je typu INT")
+    print(f"Zadané číslo: {parametr1} je typu {type(parametr1)}")
+
+if __name__ == "__main__":
+    a=2.5
+    b=28
+    c=45,8
+    d=-27
+    e=-45.8
+    f=-48,8
+    g='2.5'
+    h='28'
+    i='45,8'
+    j='-27'
+    k='-45.8'
+    l='-48,8'
+    urcityp(a)
+    urcityp(b)
+    urcityp(c)
+    urcityp(d)
+    urcityp(e)
+    urcityp(f)
+    urcityp(g)
+    urcityp(h)
+    urcityp(i)
+    urcityp(j)
+    urcityp(k)
+    urcityp(l)
