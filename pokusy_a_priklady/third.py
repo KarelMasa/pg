bfb4a1e352710bda12f1895e5d6cf675f80424dc
KarelMasa <masaka00@jcu.cz>
2024-10-28 12:35:24 +0100

    t

--- pokusy_a_priklady/third.py
+++ pokusy_a_priklady/third.py
@@ -13,6 +13,25 @@ Upravte i druhou funkci vrat_prvocisla, které pro zadané maximum vrátí sezna
 """
 from math import sqrt
 
+def urcityp_vrat_pouze_int(parametr1):
+    print(f"Zadané číslo: {parametr1} je typu {type(parametr1)}")
+    cislo_typ = type(parametr1)
+    if cislo_typ == int:
+        return int(parametr1)
+    elif cislo_typ == str:
+        text = parametr1
+        if ',' in text:
+            #cislo1 = tuple(text)
+            print(f"Zadané číslo: {parametr1} se převádí jako typ 'tuple'")
+        else:
+            if '.' in text:
+                #cislo1 = float(text)
+                print(f"Zadané číslo: {parametr1} se převádí jako typ 'float'")
+            else: 
+                cislo1 = int(text)
+                print(f"Zadané číslo: {parametr1} se převádí jako typ 'int'")
+                return cislo1
+
 def je_prvocislo(cislo):
     cislo_prv = 0
     cislo_typ = type(cislo)
--- pokusy_a_priklady/typ_promenne.py
+++ pokusy_a_priklady/typ_promenne.py
@@ -1,23 +1,27 @@
-def urcityp(parametr1):
+def urcityp_vrat_pouze_int(parametr1):
     #if type(parametr1) == int: print(f"Zadané číslo: {parametr1} je typu INT")
     print(f"Zadané číslo: {parametr1} je typu {type(parametr1)}")
     cislo_typ = type(parametr1)
-    if cislo_typ == str:
+    if cislo_typ == int:
+        return int(parametr1)
+    elif cislo_typ == str:
         text = parametr1
         if ',' in text:
             cislo1 = tuple(text)
-            print(f"Zadané číslo: {parametr1} je typu {type(cislo1)}")
+            print(f"Zadané číslo: {parametr1} se převádí jako typ 'tuple'")
         else:
             if '.' in text:
                 cislo1 = float(text)
-                print(f"Zadané číslo: {parametr1} je typu {type(cislo1)}")
+                print(f"Zadané číslo: {parametr1} se převádí jako typ 'float'")
             else: 
                 cislo1 = int(text)
-                print(f"Zadané číslo: {parametr1} je typu {type(cislo1)}")
+                print(f"Zadané číslo: {parametr1} se převádí jako typ 'int'")
+                return cislo1
     
     
 
 if __name__ == "__main__":
+    hodnota = None
     a=2.5
     b=28
     c=45,8
@@ -32,15 +36,19 @@ if __name__ == "__main__":
     l='-48,8'
     #urcityp(j)
     print("---------------------------------")
-    urcityp(a)
-    urcityp(b)
-    urcityp(c)
-    urcityp(d)
-    urcityp(e)
-    urcityp(f)
-    urcityp(g)
-    urcityp(h)
-    urcityp(i)
-    urcityp(j)
-    urcityp(k)
+    hodnota = urcityp_vrat_pouze_int(b)
+    print(hodnota)
+
+"""    
+    hodnota = urcityp_vrat_pouze_int(b)
+    hodnota = urcityp_vrat_pouze_int(c)
+    hodnota = urcityp_vrat_pouze_int(d)
+    hodnota = urcityp_vrat_pouze_int(e)
+    hodnota = urcityp_vrat_pouze_int(f)
+    hodnota = urcityp_vrat_pouze_int(g)
+    hodnota = urcityp_vrat_pouze_int(h)
+    hodnota = urcityp_vrat_pouze_int(i)
+    hodnota = urcityp_vrat_pouze_int(j)
+    hodnota = urcityp_vrat_pouze_int(k)
+"""
 
