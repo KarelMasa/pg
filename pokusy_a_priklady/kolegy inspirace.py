e34fa57f3324e589fd0595ba3b40a3295c9bfd33
KarelMasa <masaka00@jcu.cz>
2024-12-02 00:23:39 +0100

    prace se soubory

--- pokusy_a_priklady/kolegy inspirace.py
+++ pokusy_a_priklady/kolegy inspirace.py
@@ -1,4 +1,3 @@
-
 import sys
 
 # definice úvodních binárních sekvencí obrázkových souborů
@@ -7,18 +6,16 @@ gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
 
-
+#28.11.2024 VRBAT - implementation
 def read_header(file_name, header_length):
-    with open(file_name, "rb") as soubor:
-        fileheader = soubor.read(header_length)
-        #if fileheader == jpeg_header: return 
-    
     """
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return fileheader
-    
+    with open(file_name, "rb") as file_name: 
+        header_length = file_name.read(8) #přečte prvních 8 bajtů z hlavičky souboru [.jpeg = 3 bytes; .gif = 6 bytes; .png = 8 bytes]
+
+    return header_length
 
 
 def is_jpeg(file_name):
@@ -28,23 +25,27 @@ def is_jpeg(file_name):
     """
     # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
-    # vyhodnoť zda je soubor jpeg
-    if jpeg_header == header: return True
-    return False
 
+    if jpeg_header in header:  #vyhodnoť zda je soubor jpeg
+        return True
+    else:
+        return False
+  
 
 def is_gif(file_name):
     """
     Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
-    # vyhodnoť zda je soubor gif
+    #.gif má dva standardy pro zápis formátu v bin
     header = read_header(file_name, len(gif_header1))
-    # vyhodnoť zda je soubor jpeg
-    if gif_header1 == header or gif_header2 == header: return True
-
-    return False
-
+    if gif_header1 in header: #vyhodnoť zda je soubor gif
+        return True
+    elif gif_header2 in header: #vyhodnoť zda je soubor gif2
+        return True
+    else:
+        return False
+    
 
 def is_png(file_name):
     """
@@ -53,10 +54,10 @@ def is_png(file_name):
     """
     # vyhodnoť zda je soubor png
     header = read_header(file_name, len(png_header))
-    # vyhodnoť zda je soubor jpeg
-    if png_header == header: return True
-
-    return False
+    if png_header in header: #vyhodnoť zda je soubor png
+        return True
+    else:
+        return False
 
 
 def print_file_type(file_name):
@@ -74,16 +75,13 @@ def print_file_type(file_name):
 
 
 if __name__ == '__main__':
-    try:
-        #file_name = sys.argv[1]
-        file_name = "kitten.*"
-        print_file_type(file_name)
-    except IndexError:
-        print("Nebyly zadany soubory")
-    except FileNotFoundError:
-        print(f"Soubor neexistuje")
-    except OSError:
-        print("neumime hvezdickovou konvenci")
-
     # přidej try-catch blok, odchyť obecnou vyjímku Exception a vypiš ji
+    try: 
+        file_name = sys.argv[1]
+        print_file_type(file_name)
 
+    except FileNotFoundError:
+        print(f'Soubor {sys.argv[1]} nenalezen.')
+
+    except Exception:
+        print("Byla vyvolána výjimka")
